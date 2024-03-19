accea00425a986f5b488b2a364f0834f9f1a2b5a
Luiza Ehrenberger <luiafe@al.insper.edu.com>
2024-03-19 06:46:59 +0000

    aaaaaaa

--- exe1/main.c
+++ exe1/main.c
@@ -10,19 +10,24 @@
 #include "hardware/adc.h"
 
 void adc_1_task(void *p) {
+    adc_init();
+    adc_gpio_init(27); // Inicializa GPIO 27 para ADC
+    adc_gpio_init(26); // Inicializa GPIO 26 para ADC
+
+    // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
     const float conversion_factor = 3.3f / (1 << 12);
-    uint16_t result;
 
+    uint16_t result1, result2;
     while (1) {
-        adc_select_input(1); // Selecionar ADC input 1 (GPIO27)
-        vTaskDelay(pdMS_TO_TICKS(5)); // Atraso para estabilização
-        result = adc_read();
-        printf("Voltage 1: %f V\n", result * conversion_factor);
+        // Leitura ADC 1 (GP27)
+        adc_select_input(1); // Seleciona entrada ADC 1
+        result1 = adc_read();
+        printf("voltage 1: %f V\n", result1 * conversion_factor);
 
-        adc_select_input(0); // Selecionar ADC input 0 (GPIO26)
-        vTaskDelay(pdMS_TO_TICKS(5)); // Atraso para estabilização
-        result = adc_read();
-        printf("Voltage 2: %f V\n", result * conversion_factor);
+        // Leitura ADC 0 (GP26)
+        adc_select_input(0); // Seleciona entrada ADC 0
+        result2 = adc_read();
+        printf("voltage 2: %f V\n", result2 * conversion_factor);
 
         vTaskDelay(pdMS_TO_TICKS(200));
     }
@@ -31,12 +36,9 @@ void adc_1_task(void *p) {
 int main() {
     stdio_init_all();
     printf("Start RTOS \n");
-
     adc_init();
-    adc_gpio_init(27);
-    adc_gpio_init(26);
 
-    xTaskCreate(adc_1_task, "ADC_Task 1", 4095, NULL, 1, NULL);
+    xTaskCreate(adc_1_task, "ADC_Task", 4095, NULL, 1, NULL);
     vTaskStartScheduler();
 
     while (true) {
